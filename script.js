e5abc3e8222c82d460f7e2290e556b8e264da04f
Gabriel <brunengo.gabriel@gmail.com>
2021-05-22 23:12:10 -0300

    update

--- script.js
+++ script.js
@@ -11,6 +11,31 @@ let containerArray = Array.from(container.children);
 let boxCount = 0;
 let videoHeight = 480;
 let videoWidth = 854;
+let modal = document.querySelector('.modal');
+let acceptButton = document.querySelector('.accept-button');
+let cancelButton = document.querySelector('.cancel-button')
+let users = document.querySelectorAll('.user-name');
+let list = document.querySelector('.list');
+let searchInput = document.querySelector('.search-input');
+let slideContainer = document.querySelector('.slide-container');
+let slideLeft = document.querySelector('.slide-left');
+
+
+slideLeft.style.display = "none";
+slideContainer.addEventListener('mouseover',showRange);
+slideContainer.addEventListener('mouseout',hideRange);
+
+function showRange(){
+	slideLeft.style.display = "";
+	console.log("aa")
+}
+
+function hideRange(){
+	slideLeft.style.display = "none";
+}
+
+windowButton.addEventListener('click', showWindow);
+cancelButton.addEventListener('click', closeWindow);
 
 slider.addEventListener('input', (e) => {
     videoHeight = e.target.value;
@@ -23,12 +48,6 @@ slider.addEventListener('input', (e) => {
     })
 })
 
-let modal = document.querySelector('.modal');
-let acceptButton = document.querySelector('.accept-button');
-let cancelButton = document.querySelector('.cancel-button').addEventListener('click', closeWindow);
-let users = document.querySelectorAll('.user-name');
-let list = document.querySelector('.list');
-let searchInput = document.querySelector('.search-input');
 
 acceptButton.addEventListener('click', ()=>{
     let user = searchInput.value;
@@ -41,19 +60,6 @@ users.forEach((user) => {
         addVideo(e.target.textContent)
     });
 })
-
-windowButton.addEventListener('click', showWindow);
-
-function closeWindow() {
-    modal.style.display = "none";
-}
-function showWindow() {
-    modal.style.display = "flex";
-}
-
-function removeVideo() {
-    this.parentNode.remove()
-}
 
 function addVideo(user) {
     
@@ -69,11 +75,12 @@ function addVideo(user) {
     boxCount++;
     box.id = "box" + boxCount;
 
-    new Twitch.Player(box, {
+    let player = new Twitch.Player(box, {
         channel: user,
         height: videoHeight,
         width: videoWidth
     });
+	player.setVolume(0.5);
     box.appendChild(deleteButton);
     container.appendChild(box);
     containerArray = Array.from(container.children);
@@ -81,4 +88,13 @@ function addVideo(user) {
     closeWindow();
 }
 
+function closeWindow() {
+    modal.style.display = "none";
+}
+function showWindow() {
+    modal.style.display = "flex";
+}
+function removeVideo() {
+    this.parentNode.remove()
+}
 
